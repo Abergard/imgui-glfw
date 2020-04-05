d3a3cef898a87a972ec8b35f0d32a355a01d75e6
Abergard <littlinker@gmail.com>
2020-04-05 18:03:06 +0200

    [travis] export CC CXX

--- examples/main.cpp
+++ examples/main.cpp
@@ -23,7 +23,7 @@ public:
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-        window = glfwCreateWindow(640, 480, "Only4Test", nullptr, nullptr);
+        window = glfwCreateWindow(640, 480, "ImGuiGlfwOpengl3", nullptr, nullptr);
         if (!window)
         {
             glfwTerminate();
@@ -72,43 +72,16 @@ int main()
     GlfwWindow window;
     ImGuiGlfw ui{window.data()};
 
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
     while (window.is_open())
     {
         window.handle_events();
         window.draw([&]() {
             ui.draw([&]() {
-                static float f = 0.0f;
-                static int counter = 0;
 
                 ImGui::Begin("Hello, world!"); // Create a window called "Hello,
                                                // world!" and append into it.
 
                 ImGui::Text("This is some useful text.");          // Display some text (you can
-                                                                   // use a format strings too)
-                ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our
-                                                                   // window open/close state
-                ImGui::Checkbox("Another Window", &show_another_window);
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a
-                                                                        // color
-
-                if (ImGui::Button("Button")) // Buttons return true when
-                                             // clicked (most widgets return
-                                             // true when edited/activated)
-                {
-                    counter++;
-                }
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
-
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                            1000.0f / ImGui::GetIO().Framerate,
-                            ImGui::GetIO().Framerate);
                 ImGui::End();
             });
         });
